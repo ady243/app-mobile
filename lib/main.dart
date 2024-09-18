4e5efcca70f54c0359974a49957f1286be170af0
ady243 <masiviady7@gmail.com>
2024-09-18 10:54:28 +0200

    register page end loginPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:teamup/entry/entry_point.dart';
+import 'package:teamup/pages/login_page.dart';
+import 'package:teamup/pages/signup_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home:SignupPage(),
     );
   }
 }
