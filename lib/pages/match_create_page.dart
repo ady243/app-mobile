fa6a833026933e38d83b0b4e28ff631caa02d337
ady243 <masiviady7@gmail.com>
2024-10-18 18:41:37 +0200

    push add match

--- lib/pages/match_create_page.dart
+++ lib/pages/match_create_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+import 'package:intl/intl.dart'; // Pour le format de date
 
 class CreateMatchPage extends StatefulWidget {
   const CreateMatchPage({Key? key}) : super(key: key);
@@ -17,106 +17,107 @@ class _CreateMatchPageState extends State<CreateMatchPage> {
   final TextEditingController _scoreTeam1Controller = TextEditingController();
   final TextEditingController _scoreTeam2Controller = TextEditingController();
 
-
+  // Fonction pour ouvrir le panneau modal depuis le bas
   void _openBottomSheet(BuildContext context) {
     showModalBottomSheet(
       context: context,
+      isScrollControlled: true, // Permet au bottom sheet de s'ajuster
       builder: (BuildContext context) {
-        return Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min, // Pour que le popup s'adapte à son contenu
-            children: [
-              const Text(
-                'Créer un match',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 16),
-              TextField(
-                controller: _descriptionController,
-                decoration: const InputDecoration(hintText: 'Description'),
-              ),
-              const SizedBox(height: 8),
-              GestureDetector(
-                onTap: () async {
-                  final DateTime? pickedDate = await showDatePicker(
-                    context: context,
-                    initialDate: _matchDate ?? DateTime.now(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2101),
-                  );
-                  if (pickedDate != null && pickedDate != _matchDate) {
-                    setState(() {
-                      _matchDate = pickedDate;
-                    });
-                  }
-                },
-                child: InputDecorator(
-                  decoration: const InputDecoration(
-                    hintText: 'Sélectionner la date',
-                  ),
-                  child: Text(
-                    _matchDate != null ? DateFormat('yyyy-MM-dd').format(_matchDate!) : 'Date',
+        return SingleChildScrollView( // Ajout du SingleChildScrollView
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min, // Pour que le popup s'adapte à son contenu
+              children: [
+                const Text(
+                  'Créer un match',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
                   ),
                 ),
-              ),
-              const SizedBox(height: 8),
-              GestureDetector(
-                onTap: () async {
-                  final TimeOfDay? pickedTime = await showTimePicker(
-                    context: context,
-                    initialTime: _matchTime ?? TimeOfDay.now(),
-                  );
-                  if (pickedTime != null && pickedTime != _matchTime) {
-                    setState(() {
-                      _matchTime = pickedTime;
-                    });
-                  }
-                },
-                child: InputDecorator(
-                  decoration: const InputDecoration(
-                    hintText: 'Sélectionner l\'heure',
-                  ),
-                  child: Text(
-                    _matchTime != null ? _matchTime!.format(context) : 'Heure',
+                const SizedBox(height: 16),
+                TextField(
+                  controller: _descriptionController,
+                  decoration: const InputDecoration(hintText: 'Description'),
+                ),
+                const SizedBox(height: 8),
+                GestureDetector(
+                  onTap: () async {
+                    final DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: _matchDate ?? DateTime.now(),
+                      firstDate: DateTime(2000),
+                      lastDate: DateTime(2101),
+                    );
+                    if (pickedDate != null && pickedDate != _matchDate) {
+                      setState(() {
+                        _matchDate = pickedDate;
+                      });
+                    }
+                  },
+                  child: InputDecorator(
+                    decoration: const InputDecoration(
+                      hintText: 'Sélectionner la date',
+                    ),
+                    child: Text(
+                      _matchDate != null ? DateFormat('yyyy-MM-dd').format(_matchDate!) : 'Date',
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 8),
-              TextField(
-                controller: _addressController,
-                decoration: const InputDecoration(hintText: 'Adresse'),
-              ),
-              const SizedBox(height: 8),
-              TextField(
-                controller: _numberOfPlayersController,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(hintText: 'Nombre de joueurs'),
-              ),
-              const SizedBox(height: 8),
-              TextField(
-                controller: _scoreTeam1Controller,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(hintText: 'Score Équipe 1'),
-              ),
-              const SizedBox(height: 8),
-              TextField(
-                controller: _scoreTeam2Controller,
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(hintText: 'Score Équipe 2'),
-              ),
-              const SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  // Ici, tu peux ajouter la logique pour sauvegarder les données
-                  Navigator.of(context).pop(); // Fermer le panneau après la sauvegarde
-                },
-                child: const Text('Créer le match'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFF01BF6B),
+                const SizedBox(height: 8),
+                GestureDetector(
+                  onTap: () async {
+                    final TimeOfDay? pickedTime = await showTimePicker(
+                      context: context,
+                      initialTime: _matchTime ?? TimeOfDay.now(),
+                    );
+                    if (pickedTime != null && pickedTime != _matchTime) {
+                      setState(() {
+                        _matchTime = pickedTime;
+                      });
+                    }
+                  },
+                  child: InputDecorator(
+                    decoration: const InputDecoration(
+                      hintText: 'Sélectionner l\'heure',
+                    ),
+                    child: Text(
+                      _matchTime != null ? _matchTime!.format(context) : 'Heure',
+                    ),
+                  ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 8),
+                TextField(
+                  controller: _addressController,
+                  decoration: const InputDecoration(hintText: 'Adresse'),
+                ),
+                const SizedBox(height: 8),
+                TextField(
+                  controller: _numberOfPlayersController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(hintText: 'Nombre de joueurs'),
+                ),
+
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    // Ici, tu peux ajouter la logique pour sauvegarder les données
+                    Navigator.of(context).pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF01BF6B),
+                  ),
+                  child: const Text(
+                      'Créer le match',
+                      style: TextStyle(
+                          color: Colors.white
+                      ),
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
