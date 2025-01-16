d749ae27031d7195176dd28f3187ccfb0fb326f0
Mohand AIT AMARA <aitamarar@gmail.com>
2025-01-16 22:55:38 +0100

    Ajout de sécurité et page 404

--- web/main_web.dart
+++ web/main_web.dart
@@ -5,9 +5,30 @@ import 'backoffice/event_management_page.dart';
 import 'widgets/auth_guard.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: _buildTheme(),
+      initialRoute: '/loginAnalyst',
+      routes: {
+        '/loginAnalyst': (context) => const LoginPageAnalyst(),
+        '/analystDashboard': (context) => AuthGuard(
+          builder: (_) => const AnalystDashboardPage(),
+        ),
+      },
+      onGenerateRoute: _generateRoute,
+    );
+  }
+
+  ThemeData _buildTheme() {
+    return ThemeData(
       colorScheme: ColorScheme.fromSwatch(
         primarySwatch: Colors.green,
       ).copyWith(
@@ -24,24 +45,26 @@ void main() {
           textStyle: const TextStyle(fontSize: 16),
         ),
       ),
-    ),
-    initialRoute: '/loginAnalyst',
-    routes: {
-      '/loginAnalyst': (context) => const LoginPageAnalyst(),
-      '/analystDashboard': (context) => AuthGuard(
-        builder: (_) => const AnalystDashboardPage(),
+    );
+  }
+
+  Route? _generateRoute(RouteSettings settings) {
+    if (settings.name!.startsWith('/eventManagement/')) {
+      final matchId = settings.name!.split('/').last;
+      return MaterialPageRoute(
+        builder: (context) => AuthGuard(
+          builder: (_) => EventManagementPage(matchId: matchId),
+        ),
+      );
+    }
+
+    // Route inconnues
+    return MaterialPageRoute(
+      builder: (context) => Scaffold(
+        body: Center(
+          child: Text('404: Page Not Found\nRoute: ${settings.name}'),
+        ),
       ),
-    },
-    onGenerateRoute: (settings) {
-      if (settings.name!.startsWith('/eventManagement/')) {
-        final matchId = settings.name!.split('/').last;
-        return MaterialPageRoute(
-          builder: (context) => AuthGuard(
-            builder: (_) => EventManagementPage(matchId: matchId),
-          ),
-        );
-      }
-      return null;
-    },
-  ));
+    );
+  }
 }
